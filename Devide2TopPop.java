ebf8360fbd256b9639413321ff35c031f50779f3
i405817461 <likeliurui@qq.com>
2015-04-24 10:29:44 +0800

    Devide2TopPop:little fixed

--- Devide2TopPop.java
+++ Devide2TopPop.java
@@ -24,9 +24,8 @@ public class Devide2TopPop {
 	public static final String COM_PATH = "E:/doc/lab/dataset/recommending/movielens-100k/forcastPopByRecom/";
 
 	public static void main(String[] args) {
-		// 给last进行处理
-		for (int id = 1; id <= 4; id = id + 1) {
-			switch (id) {
+		for (int id = 1; id <= 6; id = id + 1) {
+/*			switch (id) {
 			case 1:
 				recentSize = "all";
 				break;
@@ -41,64 +40,16 @@ public class Devide2TopPop {
 				break;
 			default:
 				break;
-			}
+			}*/
 
-/*			PopfileName = COM_PATH +  "Pop_of_"+id+"recListSize_popAcd.txt";
+			PopfileName = COM_PATH + "origin/Pop_of_"+id+"recListSize_popAcd.txt";
 			ChoosefileName = COM_PATH  + "uUse_ItemAcd2592000.txt";
-			path = COM_PATH +id+"/";*/
-
-			PopfileName = COM_PATH + "Use_"+recentSize+"_trend.txt";
-			ChoosefileName = COM_PATH  + "uUse_ItemAcd2592000.txt";
-			path = COM_PATH + recentSize+"/";
+			path = COM_PATH + id+"/";
 			
 			for (int i = 50; i <= 800; i = i + 50) {
 				ChooseTopN ctn = new ChooseTopN(i, PopfileName, ChoosefileName,path);
 				System.out.println(ctn.topNPath);
 			}
 		}
-/*		// 给last4进行处理
-		for (int id = 2; id <= 2; id = id + 1) {
-			PopfileName = COM_PATH + id + "/last4/0.5Item_pop_dec_2th.base";
-			ChoosefileName = COM_PATH + id + "/0.5_1_10_Item_Test_4use.base";
-			path = COM_PATH + id + "/last4/";
-
-			for (int i = 50; i <= 800; i = i + 50) {
-				ChooseTopN ctn = new ChooseTopN(i, PopfileName, ChoosefileName,path);
-				System.out.println(ctn.topNPath);
-			}
-		}
-		// 给mean进行处理
-		for (int id = 2; id <= 2; id = id + 1) {
-			PopfileName = COM_PATH + id + "/mean/0.5Item_pop_dec_2th.base";
-			ChoosefileName =COM_PATH + id + "/0.5_1_10_Item_Test_4use.base";
-			path = COM_PATH + id + "/mean/";
-
-			for (int i = 50; i <= 800; i = i + 50) {
-				ChooseTopN ctn = new ChooseTopN(i, PopfileName, ChoosefileName,path);
-				System.out.println(ctn.topNPath);
-			}
-		}
-		// 给true进行处理
-		for (int id = 2; id <= 2; id = id + 1) {
-			PopfileName = COM_PATH + id + "/true/0.5Item_pop_dec_2th.base";
-			ChoosefileName = COM_PATH + id + "/0.5_1_10_Item_Test_4use.base";
-			path = COM_PATH + id + "/true/";
-
-			for (int i = 50; i <= 800; i = i + 50) {
-				ChooseTopN ctn = new ChooseTopN(i, PopfileName, ChoosefileName,path);
-				System.out.println(ctn.topNPath);
-			}
-		}
-		// 给predict进行处理
-		for (int id = 2; id <= 2; id = id + 1) {
-			PopfileName = COM_PATH + id + "/predict/0.5Item_pop_dec_2th.base";
-			ChoosefileName = COM_PATH + id + "/0.5_1_10_Item_Test_4use.base";
-			path = COM_PATH + id + "/predict/";
-
-			for (int i = 50; i <= 800; i = i + 50) {
-				ChooseTopN ctn = new ChooseTopN(i, PopfileName, ChoosefileName,path);
-				System.out.println(ctn.topNPath);
-			}
-		}*/
 	}
 }
